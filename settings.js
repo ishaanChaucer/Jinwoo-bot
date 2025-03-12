e9f3ae68fc7f9760d3a2a34a97e2f5cfe7ddb0f3
ishaanChaucer <ishaanchaucer@proton.me>
2025-03-13 01:36:00 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'MALVIN-XD~plYDVKrZ#Bq5U3Waiv3E4GS48JlsI1wMtQp-aA6j1SQXCD8SYcHk' 
 //Enter your Jinwoo session id here; must start with MALVIN-XD~
 
 //--------------------[ BOT NAME ]----------------------//
